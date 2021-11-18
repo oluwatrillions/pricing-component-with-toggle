cb305a5962b211ef77cc8c82b18efb96950ccf84
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2021-11-19 00:11:07 +0100

    Toggle button

---
+++ pricing.js
@@ -0,0 +1,6 @@
+const ball = document.querySelector('.ball')
+const container = document.querySelector('.midDiv')
+
+ball.addEventListener('click', function () {
+    ball.classList.toggle('ball-toggle')
+});
