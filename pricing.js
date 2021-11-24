e58c9561273a91d5b8bd6a9ec515012023e3dc6c
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2021-11-24 03:46:06 +0100

    toggle ball to active state

--- pricing.js
+++ pricing.js
@@ -1,6 +1,8 @@
 const ball = document.querySelector('.ball')
 const container = document.querySelector('.midDiv')
+const firstPrice = document.querySelector('.firstPrice')
 
 ball.addEventListener('click', function () {
     ball.classList.toggle('ball-toggle')
 });
+
