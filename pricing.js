3f3fcdb99bdece2d35b75b10b63a42075906cbb4
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2021-11-24 14:25:22 +0100

    final changes

--- pricing.js
+++ pricing.js
@@ -1,8 +1,28 @@
 const ball = document.querySelector('.ball')
 const container = document.querySelector('.midDiv')
 const firstPrice = document.querySelector('.firstPrice')
+const secondPrice = document.querySelector('.secondPrice')
+const thirdPrice = document.querySelector('.thirdPrice')
 
 ball.addEventListener('click', function () {
-    ball.classList.toggle('ball-toggle')
+    activeState();
+    
+    if (ball.classList.toggle('ball-toggle')) {
+        firstDiv();
+    }
 });
+
+let firstDiv = () => {
+    firstPrice.textContent = `$199.99`;
+    secondPrice.textContent = `$249.99`;
+    thirdPrice.textContent = `$399.99`
+}
+
+
+let activeState = () => {
+    firstPrice.textContent = `$19.99`;
+    secondPrice.textContent = `$24.99`;
+    thirdPrice.textContent = `$39.99`;
+}
+
 
